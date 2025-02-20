523884de5e0914b22f28e17235be06c319ebec0d
noormarwaha <s4080223@student.rmit.edu.au>
2025-02-20 15:31:47 -0800

    Initial Commit

---
+++ ListThemAll/ListThemAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListThemAllApp.swift
+//  ListThemAll
+//
+//  Created by Noor Marwaha on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ListThemAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
